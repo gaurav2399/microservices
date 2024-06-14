26273dff4a02708d4b15cc4816df437a711ebd63
Gaurav <gs926189@gmail.com>
2024-06-14 08:55:13 +0530

    add circuit-breaker and rate-limiter

--- currency-exchange-service/src/main/java/com/gaurav/microservices/currency_exchange_service/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/gaurav/microservices/currency_exchange_service/CircuitBreakerController.java
@@ -1,5 +1,7 @@
 package com.gaurav.microservices.currency_exchange_service;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,12 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@Retry(name = "sample-api", fallbackMethod = "hardCodeResponse")
 public class CircuitBreakerController {
 
     Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/api")
+    // @Retry(name = "sample-api", fallbackMethod = "hardCodeResponse")
+    @CircuitBreaker(name = "sample-api", fallbackMethod = "hardCodeResponse")
+    @RateLimiter(name = "sample-api", fallbackMethod = "hardCodeResponse")
     public String getSampleApi(){
         logger.info("sample api called!");
         ResponseEntity<String> responseEntity = new RestTemplate()
@@ -23,6 +27,7 @@ public class CircuitBreakerController {
     }
 
     public String hardCodeResponse(Exception e){
+        logger.info("fallback method error");
         return "Some Error";
     }
 
